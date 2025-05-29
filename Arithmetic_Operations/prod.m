276ed53bc40e52c02f7e411bf9e4da513918f172
heyeepc <2675458141@qq.com>
2025-05-29 21:56:30 +0800

    Update prod.m

--- Arithmetic_Operations/prod.m
+++ Arithmetic_Operations/prod.m
@@ -2,3 +2,10 @@ v = [1, 2, 3, 4, 5];
 p_v = prod(v);
 disp(['向量 [', num2str(v), '] 的乘积是: ', num2str(p_v)]);
 % "number to string"，也就是将数字转换为字符串
+
+M = [1, 2, 3;
+     4, 5, 6;
+     7, 8, 9];
+p_col = prod(M);
+disp('矩阵每列的乘积是:');
+disp(p_col);
